10f3602e208ce6141f0fd3c9a3b886b24c1adab6
peter720720 <oyinloyepeter273@gmail.com>
2025-12-06 22:44:10 +0100

    text change

--- menu.js
+++ menu.js
@@ -3,13 +3,13 @@ const closeBtn = document.getElementById("closeBtn");
 const navBar = document.getElementById("navBar");
 
 menuBtn.addEventListener("click", () => {
-    menuBtn.style.display = "none";      // hide menu icon
-    closeBtn.style.display = "block";    // show X
-    navBar.classList.add("active");      // slide nav in
+    menuBtn.style.display = "none";
+    closeBtn.style.display = "block";
+    navBar.classList.add("active");
 });
 
 closeBtn.addEventListener("click", () => {
-    closeBtn.style.display = "none";     // hide X
-    menuBtn.style.display = "block";     // show menu icon
-    navBar.classList.remove("active");   // slide nav out
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "block";
+    navBar.classList.remove("active");
 });
